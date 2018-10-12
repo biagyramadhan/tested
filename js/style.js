e327ecd902fe89824f43294e55d238c17fcb76f4
Biagy Ramadhan 83 <biagyramadhan01@gmail.com>
2018-10-12 13:23:20 +0700

    Initial commit

---
+++ js/style.js
@@ -0,0 +1,19 @@
+  $(function(){
+    $(".type").typed({
+      strings: [
+        "Hey.",
+        "Biagy Roosevelt"
+      ],
+      typeSpeed : 155,
+      backDelay : 500,
+      loop:true
+    })
+  })
+  $(function(){
+    $(".type2").typed({
+      strings: [
+        "Web Design | Student | Web Backend "
+      ],
+      typeSpeed : 200
+    })
+  })
